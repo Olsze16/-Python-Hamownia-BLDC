00b99c28b098fda459274c45225308f946d9c7b8
Olsze16 <59530245+Olsze16@users.noreply.github.com>
2020-07-20 21:27:36 +0200

    dynamicplots + sensors updated
    first connection with STM32

--- hamownia/mainwindow.py
+++ hamownia/mainwindow.py
@@ -7,112 +7,144 @@ from matplotlib import pyplot as plt
 from plotsensors import calculatesensors, updateplots
 import matplotlib.animation as animation
 import styledef
+import time
+from PIL import ImageTk
 
 plt.style.use('dark_background')
 #Private variables
 i=0
 data_int=[]
 sensorsread=False
+dynamicplot=False
 x=[]
 y=[]
+ping =0.1
 
 
 #Private variables END
+def dynamicplotbox():
+    global dynamicplot
+    dynamicplot = not dynamicplot
+    return dynamicplot
 def showplottest_clicked():
-    x_len = 200  # Number of points to display
-    y_range = [0, 85]  # Range of possible Y values to display
-    fig, (ax,ax1, ax2, ax3, ax4) = plt.subplots(nrows=5, sharex=True)
-    ax.set_ylabel("[mA]")
-    ax1.set_ylabel("[V]")
-    ax2.set_ylabel("[rpm]")
-    ax3.set_ylabel("oC]")
-    ax4.set_ylabel("[g]")
-    xs = list(range(0, 200))
-    ys = [0] * x_len
-    ys1 = [0] * x_len
-    ys2 = [0] * x_len
-    ys3 = [0] * x_len
-    ys4 = [0] * x_len
-    ax.set_ylim(y_range)
-    ax1.set_ylim(y_range)
-    ax2.set_ylim(y_range)
-    ax3.set_ylim(y_range)
-    ax4.set_ylim(y_range)
-    line, = ax.plot(xs, ys)
-    line1, = ax1.plot(xs, ys1)
-    line2, = ax2.plot(xs, ys2)
-    line3, = ax3.plot(xs, ys3)
-    line4, = ax4.plot(xs, ys4)
+    if (dynamicplot==True):
+        x_len = 200  # Number of points to display
+        y_range = [0, 35]  # Range of possible Y values to display
+        fig, (ax,ax1, ax2, ax3, ax4) = plt.subplots(nrows=5, sharex=True)
+        ax.set_ylabel("[mA]")
+        ax1.set_ylabel("[V]")
+        ax2.set_ylabel("[rpm]")
+        ax3.set_ylabel("oC]")
+        ax4.set_ylabel("[g]")
+        xs = list(range(0, 200))
+        ys = [0] * x_len
+        ys1 = [0] * x_len
+        ys2 = [0] * x_len
+        ys3 = [0] * x_len
+        ys4 = [0] * x_len
+        ax.set_ylim(y_range)
+        ax1.set_ylim(y_range)
+        ax2.set_ylim(y_range)
+        ax3.set_ylim(y_range)
+        ax4.set_ylim(y_range)
+        line, = ax.plot(xs, ys)
+        line1, = ax1.plot(xs, ys1)
+        line2, = ax2.plot(xs, ys2)
+        line3, = ax3.plot(xs, ys3)
+        line4, = ax4.plot(xs, ys4)
 
-    plt.xlabel('Samples')
+        plt.xlabel('Samples')
 
-    def animate(i, ys, ys1, ys2, ys3, ys4):
-        try:  # sprawdzenie czy socket dostaje dane
-            dataplot = soc.recv(1024 * 10)
-            a_list = dataplot.split()
-            map_object = map(int, a_list)
-            data_intplot = list(map_object)
-            ys.append(float(data_intplot[0]))
-            ys1.append(float(data_intplot[1]))
-            ys2.append(float(data_intplot[2]))
-            ys3.append(float(data_intplot[3]))
-            ys4.append(float(data_intplot[4]))
-            ys = ys[-x_len:]
-            ys1 = ys1[-x_len:]
-            ys2 = ys2[-x_len:]
-            ys3 = ys3[-x_len:]
-            ys4 = ys4[-x_len:]
-            line.set_ydata(ys)
-            line1.set_ydata(ys1)
-            line2.set_ydata(ys2)
-            line3.set_ydata(ys3)
-            line4.set_ydata(ys4)
-            return line, line1, line2, line3, line4,
-        except socket.error:  # jezeli socket nie dostaje danych:
-            ys.append(float(0))
-            ys1.append(float(0))
-            ys2.append(float(0))
-            ys3.append(float(0))
-            ys4.append(float(0))
-            ys = ys[-x_len:]
-            ys1 = ys1[-x_len:]
-            ys2 = ys2[-x_len:]
-            ys3 = ys3[-x_len:]
-            ys4 = ys4[-x_len:]
-            line.set_ydata(ys)
-            line1.set_ydata(ys1)
-            line2.set_ydata(ys2)
-            line3.set_ydata(ys3)
-            line4.set_ydata(ys4)
-            return line, line1, line2, line3, line4,
-            pass  # zwolnienie programu w momencie, kiedy socket nie dostaje danych
-    ani = animation.FuncAnimation(fig, animate, fargs=(ys,ys1,ys2,ys3,ys4),interval=100,blit=True)
-    plt.show()
+        def animate(i, ys, ys1, ys2, ys3, ys4):
+            try:  # sprawdzenie czy socket dostaje dane
+                dataplot = soc.recv(27)
+                a_list = dataplot.split()
+                map_object = map(float, a_list)
+                data_intplot = list(map_object)
+                ys.append(float(data_intplot[0]))
+                ys1.append(float(data_intplot[1]))
+                ys2.append(float(data_intplot[2]))
+                ys3.append(float(data_intplot[3]))
+                ys4.append(float(data_intplot[4]))
+                if(sensorsread==True):
+                    current.set(str(data_intplot[0]) + str(" [mA]"))
+                    voltage.set(str(data_intplot[1]) + str(" [V]"))
+                    rpm.set(str(data_intplot[2]) + str(" [rpm]"))
+                    temp.set(str(data_intplot[3]) + str(" [C]"))
+                    ciag.set(str(data_intplot[4]) + str(" [g]"))
+                ys = ys[-x_len:]
+                ys1 = ys1[-x_len:]
+                ys2 = ys2[-x_len:]
+                ys3 = ys3[-x_len:]
+                ys4 = ys4[-x_len:]
+                line.set_ydata(ys)
+                line1.set_ydata(ys1)
+                line2.set_ydata(ys2)
+                line3.set_ydata(ys3)
+                line4.set_ydata(ys4)
+                return line, line1, line2, line3, line4,
+            except socket.error:  # jezeli socket nie dostaje danych:
+                ys.append(float(0))
+                ys1.append(float(0))
+                ys2.append(float(0))
+                ys3.append(float(0))
+                ys4.append(float(0))
+                if(sensorsread==True):
+                    current.set(str("0") + str(" [mA]"))
+                    voltage.set(str("0") + str(" [V]"))
+                    rpm.set(str("0") + str(" [rpm]"))
+                    temp.set(str("0") + str(" [C]"))
+                    ciag.set(str("0") + str(" [g]"))
+                ys = ys[-x_len:]
+                ys1 = ys1[-x_len:]
+                ys2 = ys2[-x_len:]
+                ys3 = ys3[-x_len:]
+                ys4 = ys4[-x_len:]
+                line.set_ydata(ys)
+                line1.set_ydata(ys1)
+                line2.set_ydata(ys2)
+                line3.set_ydata(ys3)
+                line4.set_ydata(ys4)
+                return line, line1, line2, line3, line4,
+                pass  # zwolnienie programu w momencie, kiedy socket nie dostaje danych
+        ani = animation.FuncAnimation(fig, animate, fargs=(ys,ys1,ys2,ys3,ys4),interval=85,blit=True)
+        plt.show()
+    elif(sensorsread==True and dynamicplot==False):
+        current.set(str("0") + str(" [mA]"))
+        voltage.set(str("0") + str(" [V]"))
+        rpm.set(str("0") + str(" [rpm]"))
+        temp.set(str("0") + str(" [C]"))
+        ciag.set(str("0") + str(" [g]"))
+        while(sensorsread==True):
+            receivedata()
 def changesensorsvar():
     global sensorsread
     sensorsread = not sensorsread
     return sensorsread
 def receivedata():
-    global data_int, ax
     mainwindow.update()
+    time.sleep(ping)
     try: #sprawdzenie czy socket dostaje dane
-        data = soc.recv(1024 * 10)
-        a_list = data.split()
-        map_object = map(int, a_list)
-        data_int = list(map_object)
-        current.set(str(data_int[0]) + str(" [mA]"))
-        voltage.set(str(data_int[1]) + str(" [V]"))
-        rpm.set(str(data_int[2]) + str(" [rpm]"))
-        temp.set(str(data_int[3]) + str(" [C]"))
-        ciag.set(str(data_int[4]) +str(" [g]"))
+        dataplot = soc.recv(27)
+        print(dataplot)
+        a_list = dataplot.split()
+        map_object = map(float, a_list)
+        data_intplot = list(map_object)
+        current.set(str(data_intplot[0]) + str(" [mA]"))
+        voltage.set(str(data_intplot[1]) + str(" [V]"))
+        rpm.set(str(data_intplot[2]) + str(" [rpm]"))
+        temp.set(str(data_intplot[3]) + str(" [C]"))
+        ciag.set(str(data_intplot[4]) +str(" [g]"))
     except socket.error: #jezeli socket nie dostaje danych:
         pass #zwolnienie programu w momencie, kiedy socket nie dostaje danych
 def gauss1():
     import gauss
 def connectbutton_clicked():
-    global UDP_ip, UDP_port, soc
+    global UDP_ip, UDP_port, soc, servIP, servPORT
     UDP_ip=entryIP.get()
     UDP_port=int(entryport.get())
+    servIP=servIP.get()
+    servPORT=int(servPORT.get())
     soc=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     soc.bind((UDP_ip,UDP_port))
     soc.setblocking(0)
@@ -120,6 +152,9 @@ def connectbutton_clicked():
     mainwindowwidget()
 def startbutton_clicked():
     global i
+    soc.sendto(bytes("1",  "utf-8"), (servIP, servPORT))
+    showplottest_clicked()
+    print("Gruba jazdeczka")
     i+=1
 def testbutton_clicked():
     tk.messagebox.showerror(title="Connection test", message="STM32 not connected" + str(data_int[0]))
@@ -128,17 +163,20 @@ def mainwindowwidget():
     global mainwindow
     mainwindow = tk.Tk()
     styledef.mainwindowstyle()
-    mainwindow.configure(bg="#bdc3c7")
     mainwindow.title("BLDC")
-    putlogo=tk.PhotoImage(file="put.png")
-    tk.Label(mainwindow, image=putlogo, background="#bdc3c7", relief="solid").grid(row=1, column=2)
+    mainwindow.configure(bg="#bdc3c7")
     tk.Label(mainwindow,bg="#bdc3c7", text="Sterowanie hamownią silników BLDC - Nucelo F7", font=("Helvetica",16, "bold")).grid(row=0,column=2)
     tk.Label(mainwindow, text="", bg="#bdc3c7").grid(row=2, column=2)
     tk.Label(mainwindow,bg="#bdc3c7", text="Podłączono z sięcią \n Ethernet UDP \n Adres IP:"+ UDP_ip +"\nPort:" + str(UDP_port), relief="solid", font=("Arial",8)).grid(row=1, column=5, columnspan=2)
 
     #Konfiguracja przycisków do obsługi stringów wysyłanych do STM32-F7
     ttk.Label(mainwindow, text="Sygnały sterujące:", style="BW.TLabel").grid(row=3, column=2)
+    putlogo1=tk.PhotoImage(file='putlogo.png')
+    imagelabel=tk.Label(mainwindow, image=putlogo1, background="#bdc3c7", relief="solid").grid(row=1, column=2)
+    ttk.Separator(mainwindow).grid(row=4, column=0, columnspan=7, ipadx=400)
+    ttk.Separator(mainwindow).grid(row=10, column=0, columnspan=7, ipadx=400)
     ttk.Button(mainwindow,text="Start BLDC", style="But1.TButton", command=startbutton_clicked).grid(row=5,column=2)
+    tk.Checkbutton(mainwindow, text="Show dynamic plots\n during test:",font=("Helvetica",9), command=dynamicplotbox,bg="#bdc3c7", activebackground="#bdc3c7").grid(row=11, column=2)
     ttk.Button(mainwindow,text="Stop BLDC", style="But1.TButton").grid(row=6,column=2)
     ttk.Button(mainwindow,text="Connection test", style="But1.TButton", command=testbutton_clicked).grid(row=7,column=2)
 
@@ -149,13 +187,14 @@ def mainwindowwidget():
     tk.Button(mainwindow,text="Sensor 3 FFT", width=15, height=1).grid(row=7, column=0)
 
     #Konfiguracja wyświetlania pomiarów na żywo
-    tk.Checkbutton(mainwindow, text="Show sensors:", command=changesensorsvar).grid(row=4,column=5,columnspan=2)
+    ttk.Label(mainwindow, text="Odczyt sensorów:", style="BW.TLabel").grid(row=3, column=5, columnspan=2)
+    tk.Checkbutton(mainwindow, text="Show dynamic sensors\n value during test", font=("Helvetica",9),command=changesensorsvar, bg="#bdc3c7", activebackground="#bdc3c7").grid(row=11,column=5,columnspan=2)
     ttk.Label(mainwindow,text="Sensor 1:", style="BW3.TLabel").grid(row=5, column=5)
     ttk.Label(mainwindow,text="Sensor 2:", style="BW3.TLabel").grid(row=6, column=5)
     ttk.Label(mainwindow,text="Sensor 3:", style="BW3.TLabel").grid(row=7, column=5)
     ttk.Label(mainwindow,text="Sensor 4:", style="BW3.TLabel").grid(row=8, column=5)
     ttk.Label(mainwindow,text="Sensor 5:", style="BW3.TLabel").grid(row=9, column=5)
-    ttk.Button(mainwindow, text="Show plots", style="But2.TButton", command=showplottest_clicked).grid(row=10,column=5,columnspan=2, sticky="E")
+    tk.Checkbutton(mainwindow, text="Collect data for\n Gaussian FFT analysis", font=("Helvetica",9),command=changesensorsvar, bg="#bdc3c7", activebackground="#bdc3c7").grid(row=11,column=0)
     global current, voltage, rpm, temp, ciag
     current=tk.StringVar()
     voltage=tk.StringVar()
@@ -170,11 +209,8 @@ def mainwindowwidget():
     mainwindow.resizable(0, 0)
     while True:
         mainwindow.update()
-        if (sensorsread==True):
-            receivedata()
-
 def welcomewindowwidget():
-    global entryIP, entryport, welcomewindow
+    global entryIP, entryport, welcomewindow, servIP, servPORT
     welcomewindow = tk.Tk()
     styledef.welcomnewindowstyle()
     welcomewindow.title("Praca inżynierska - stanowisko pomiarowe BLDC")
@@ -187,19 +223,27 @@ def welcomewindowwidget():
     #Connection info + buttons
     ttk.Label(welcomewindow, style="BW.TLabel", text="Adres IP :").grid(row=3, column=0)
     ttk.Label(welcomewindow, style="BW.TLabel", text="Port :").grid(row=4, column=0)
+    ttk.Label(welcomewindow, style="BW.TLabel", text="Adres IP (stm32) :").grid(row=5, column=0)
+    ttk.Label(welcomewindow, style="BW.TLabel", text="Port (stm32) :").grid(row=6, column=0)
+    servIP=ttk.Entry(welcomewindow)
+    servIP.grid(row=5, column=1)
+    servIP.insert(0,"192.168.2.2")
+    servPORT=ttk.Entry(welcomewindow)
+    servPORT.grid(row=6, column=1)
+    servPORT.insert(0,"7")
     entryIP=ttk.Entry(welcomewindow)
     entryIP.grid(row=3, column=1)
     entryIP.insert(0,"192.168.0.16")
     entryport=ttk.Entry(welcomewindow)
     entryport.grid(row=4,column=1)
     entryport.insert(0,"20001")
-    ttk.Button(welcomewindow,style="But1.TButton", text="connect",command=connectbutton_clicked).grid(row=5, column=0, columnspan=2)
+    ttk.Button(welcomewindow,style="But1.TButton", text="connect",command=connectbutton_clicked).grid(row=7, column=0, columnspan=2)
 
 
     #Authors
-    ttk.Label(welcomewindow, style="BW1.TLabel", text="Autorzy pracy:").grid(row=6,column=1, sticky="E")
-    ttk.Label(welcomewindow, style="BW1.TLabel", text="Kamil Olszewski (133657)").grid(row=7, column=1, sticky="E")
-    ttk.Label(welcomewindow, style="BW1.TLabel", text="Daniel Świątek (133671)").grid(row=8, column=1, sticky="E")
+    ttk.Label(welcomewindow, style="BW1.TLabel", text="Autorzy pracy:").grid(row=8,column=1, sticky="E")
+    ttk.Label(welcomewindow, style="BW1.TLabel", text="Kamil Olszewski (133657)").grid(row=9, column=1, sticky="E")
+    ttk.Label(welcomewindow, style="BW1.TLabel", text="Daniel Świątek (133671)").grid(row=10, column=1, sticky="E")
 
     welcomewindow.resizable(0,0)
     welcomewindow.mainloop()
--- hamownia/styledef.py
+++ hamownia/styledef.py
@@ -7,7 +7,7 @@ def welcomnewindowstyle():
     buttonstyle.configure("But1.TButton", background="#27ae60", height=1, width=15)
 def mainwindowstyle():
     labelstyle = ttk.Style()
-    labelstyle.configure("BW.TLabel", foreground="black",background="#bdc3c7", font=("Helvetica",10))
+    labelstyle.configure("BW.TLabel", foreground="black",background="#bdc3c7", font=("Helvetica",11))
     labelstyle.configure("BW1.TLabel", foreground="black", background="#bdc3c7", font=("Helvetica", 7))
     labelstyle.configure("BW2.TLabel", foreground="red3",background="#bdc3c7", font=("Helvetica",10))
     labelstyle.configure("BW3.TLabel",background="#bdc3c7", font=("Helvetica",10))
@@ -15,3 +15,5 @@ def mainwindowstyle():
     buttonstyle=ttk.Style()
     buttonstyle.configure("But1.TButton", background="#27ae60", height=1, width=15)
     buttonstyle.configure("But2.TButton", background="blue", height=0.5, width=15)
+    checkbuttonstyle=ttk.Style()
+    checkbuttonstyle.configure("C1.TCheckbutton", background="bdc3c7", font=("Helvetica",10), width=15, height=1)
